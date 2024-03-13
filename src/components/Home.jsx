9ccbcb14a326c02438265aab42bce72708ef385f
BuraYu <burak.yueksel@hotmail.com>
2024-03-13 18:28:44 +0100

    add socials navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons";
+
+const Home = () => {
+  return (
+   
+    <div>
+      <h1></h1>
+    </div>;
+    )
+};
+
+export default Home;
