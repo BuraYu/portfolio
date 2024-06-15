75a37eb6c687afee61a06e81ccc0cb6b4d17094b
BuraYu <burak.yueksel@hotmail.com>
2024-06-15 23:38:28 +0200

    add skeleton of portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AboutMe from "./components/aboutMe/AboutMe";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer.jsx";
+import Header from "./components/header/Header.jsx";
+import Hero from "./components/hero/Hero.jsx";
+import Projects from "./components/projects/Projects.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <AboutMe />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/aboutMe/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./aboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h1 className="about">About Me</h1>
+    </div>
+  );
+};
+
+export default AboutMe;
