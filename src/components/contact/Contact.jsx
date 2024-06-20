14888cc2632c1ddcd8aff0f67cf3c7d9fb1a059b
BuraYu <burak.yueksel@hotmail.com>
2024-06-20 21:23:48 +0200

    changed Contact component to BEM standard

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,15 +31,32 @@ const Contact = () => {
   //design
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="contact">
+      <div className="contact__container">
+        <div className="contact__form-wrapper">
+          <h2 className="contact__header">Fill the Form. It's easy.</h2>
+          <form ref={form} onSubmit={sendEmail} className="contact__form">
+            <label className="contact__label">Name</label>
+            <input type="text" name="user_name" className="contact__input" />
+            <label className="contact__label">Email</label>
+            <input type="email" name="user_email" className="contact__input" />
+            <label className="contact__label">Message</label>
+            <textarea name="message" className="contact__textarea" />
+            <input type="submit" value="Send" className="contact__submit" />
+          </form>
+        </div>
+
+        <div className="contact__message">
+          <h3 className="contact__message-header">Hello</h3>
+          <p className="contact__message-text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+            nobis quibusdam quae beatae. Eligendi laudantium cum hic veniam est
+            dolor, mollitia fuga dolores voluptatem eius minus ut nesciunt esse
+            rem!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
